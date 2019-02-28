afecdf95a0b93c6363508257ce5129ac05fc5612
Aliaksei_Nazarau <nalex2004@gmail.com>
2019-02-28 09:55:24 +0300

    Context

---
+++ SalesEntity/Entity/SalesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace NAlex.SalesEntity.Entity
+{
+    public class SalesDbContext : DbContext
+    {
+        public SalesDbContext() : base("Sales")
+        {
+        }
+
+        public DbSet<SourceFile> SourceFiles { get; set; }
+        public DbSet<ErrorFile> ErrorFiles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
