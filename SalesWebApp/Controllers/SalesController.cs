eff72efb4a69ea7387cc2e5308e246f74aa0cd8a
Aliaksei_Nazarau <nalex2004@gmail.com>
2019-03-05 14:14:03 +0300

    Web app started

---
+++ SalesWebApp/Controllers/SalesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Sales.SaleSource.Factory;
+using Sales.SaleSource.Github;
+using Sales.SaleSource;
+
+namespace Sales.SalesWebApp.Controllers
+{
+    public class SalesController : ApiController
+    {
+        IHookConsumer hookConsumer;
+
+        public SalesController(IHookConsumer hookConsumer)
+        {
+            this.hookConsumer = hookConsumer;
+        }
+
+        [HttpPost]
+        public async Task WebHook([FromBody] string hookJson)
+        {
+            if (hookConsumer != null)
+            {
+                await hookConsumer.ConsumeHookAsync(hookJson);
+            }
+        }
+    }
+}
---
+++ SalesWebApp/SalesDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using Sales.SaleSource;
+using Sales.SalesWebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Sales.SaleSource.Factory;
+using Sales.SaleSource.Validation;
+
+namespace Sales.SalesWebApp
+{
+    public class SalesDependencyResolver : IDependencyResolver
+    {
+        public IDependencyScope BeginScope()
+        {
+            return new SalesDependencyResolver();
+        }
+
+        public void Dispose()
+        {            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(SalesController))
+            {
+                IHookConsumer hookConsumer = new GithubHookConsumer(new GithubSalesHandlerFactory(), "", fileName => FileNameValidator.Validate(fileName));
+                return new SalesController(hookConsumer);
+            }
+
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return new List<object>();
+        }
+    }
+}
