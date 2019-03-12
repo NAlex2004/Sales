7501747e53d4bace309549d296be869cb97ff61e
Aliaksei_Nazarau <nalex2004@gmail.com>
2019-03-12 11:15:03 +0300

    save to db

--- SalesFunctionApp/SalesDataSaverFunction.cs
+++ SalesFunctionApp/SalesDataSaverFunction.cs
@@ -1,16 +1,22 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Sales.SaleSource;
+using Sales.SaleSource.Factory;
 
 namespace Sales.SalesFunctionApp
 {
     public static class SalesDataSaverFunction
     {
         [FunctionName("SalesDataSaverFunction")]
-        public static void Run([QueueTrigger("SalesDataQueue")]SaleDataObtainmentResult saleDataObtainmentResult, TraceWriter log)
+        public static async Task Run([QueueTrigger("SalesDataQueue")]SaleDataObtainmentResult saleDataObtainmentResult, TraceWriter log)
         {
             log.Info($"C# Queue trigger function processed: SalesDataSaverFunction");
+
+            ISalesHandlerFactory handlerFactory = new GithubSalesHandlerFactory();
+            SalesHandlerBase salesHandler = handlerFactory.GetSalesHandler();
+            await salesHandler.HandleSaleDataAsync(saleDataObtainmentResult);
         }
     }
 }
